265d9cbb5ce356302b7e017f95e06fa4bf083ba3
Zuhri Nurhuda <zuhri.nurhuda@gmail.com>
2017-10-10 16:13:22 +0700

    routes setup done

---
+++ app.js
@@ -0,0 +1,30 @@
+// require express
+const express = require('express')
+const app = express()
+
+// require body-parser
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// require ejs
+app.set('view engine', 'ejs')
+
+// route index
+const index = require('./routers/index')
+app.use('/', index)
+
+// route user
+const user = require('./routers/user')
+app.use('/user', user)
+
+// route owner
+const owner = require('./routers/owner')
+app.use('/owner', owner)
+
+// route group
+const group = require('./routers/group')
+app.use('/group', group)
+
+app.listen(3000)
+console.log('Listening on port 3000')
